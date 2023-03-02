39e595a2874dff180c795698ac093f530228dfac
Andrey Saranchin <Andrey22102001@gmail.com>
2023-03-02 10:08:25 +0300

    pgdm_bench: improve read benchmarks
    Improve existing benchmark, add new benchmarks of `get` and form a
    structure of benchmark naming.
    
    Rules of naming benchmarks:
    Every benchmark should be named `bench_{1}_{2}_{3}.lua`, where
    1. Name of method/group of methods that is/are tested.
    2. Way of benchmarking, e.g. `seq` - sequential, `prnd` - pseudo random,
       `rnd` - random.
    3. Size of data set in scientific notation: 1e4, for example.

---
+++ pgdm_bench/bench_get_seq_1e6.lua
@@ -0,0 +1,58 @@
+require('os').execute('rm *.xlog *.snap')
+clock = require('clock')
+box.cfg{}
+s = box.schema.space.create('select_tester')
+s:create_index('pk')
+sk = s:create_index('sk', {type = 'pgdm'})
+local iter_num = 1e6
+for i = 1, iter_num do
+	s:replace{i}
+end
+
+function bench_tree()
+	for i = 1, iter_num do
+		s:get{i}
+	end
+end
+
+function bench_pgdm()
+	for i = 1, iter_num do
+		sk:get{i}
+	end
+end
+
+-- Warmup
+bench_tree()
+bench_pgdm()
+
+tree = {}
+pgdm = {}
+
+exp_num = 5
+
+for i = 1, exp_num do
+	time = clock.bench(bench_tree)[1]
+	table.insert(tree, time)
+	print('Tree: ', time)
+	time = clock.bench(bench_pgdm)[1]
+	table.insert(pgdm, time)
+	print('Pgdm: ', time)
+end
+
+tree_avg_time = 0
+pgdm_avg_time = 0
+for i = 1, exp_num do
+	tree_avg_time = tree_avg_time + tree[i]
+	pgdm_avg_time = pgdm_avg_time + pgdm[i]
+end
+tree_avg_time = tree_avg_time / 5
+pgdm_avg_time = pgdm_avg_time / 5
+
+tree_rps = iter_num / tree_avg_time 
+pgdm_rps = iter_num / pgdm_avg_time 
+
+print('Tree RPS: ', tree_rps)
+print('Pgdm RPS: ', pgdm_rps)
+print('Gained ', (pgdm_rps - tree_rps) / tree_rps * 100, '%')
+
+s:drop()
