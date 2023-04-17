34c596788b6a01b88d5d1bcd5919e89de1e0fae6
Andrey Saranchin <Andrey22102001@gmail.com>
2023-04-17 12:11:14 +0300

    tmp

--- src/lib/salad/pgdm.hpp
+++ src/lib/salad/pgdm.hpp
@@ -4,225 +4,69 @@
 #include <cstdlib>
 #include "gblock.hpp"
 
-template<class Key, class Value, unsigned EPS, unsigned DELTA>
+template<class Key, unsigned EPS, unsigned DELTA, unsigned EXTENT_SIZE, unsigned BLOCK_SIZE>
 class pgdm_map {
 	static_assert(DELTA > 0U, "Delta must not be zero");
 	static_assert(EPS > 0U, "Epsilon must not be zero");
 	static_assert(std::is_trivially_copyable<Key>::value, "Key must be trivially copyable");
-	static_assert(std::is_trivially_copyable<Value>::value, "Value must be trivialy copyable");
 	/* TODO: check if Key is a numeric type. */
+
+	static_assert(EXTENT_SIZE % BLOCK_SIZE == 0, "Extent size must be divisible by block size");
 
 	template<class T>
 	using Collection = std::vector<T>;
 
-	struct INode {
-		virtual Collection<INode *>
-		insert(const Key &k, const void *v) = 0;
-		virtual bool
-		find(const Key &k, void *v) = 0;
-		virtual void
-		del(const Key &k) = 0;
-		virtual void
-		del_checked(const Key &k) = 0;
-		virtual bool
-		lower_bound(const Key &k, void *v) = 0;
-		virtual bool
-		is_leaf() = 0;
-		virtual const Key &
-		origin_key() = 0;
-		virtual const Key &
-		start_key() = 0;
-		virtual INode *
-		first_node() = 0;
-		virtual Collection<INode *>
-		get_children() = 0;
-		virtual Collection<Key>
-		get_data() = 0;
-		virtual Collection<Key>
-		get_extra() = 0;
-	};
+	using DataHolder = internal::DataHolder<Key, 2 * EPS>;
+	using DataPayload = internal::DataPayload<Key, 2 * EPS>;
+	using Node = GeometricBlock<Key, EPS, DELTA, BLOCK_SIZE>;
 
-	struct Node : INode {
-		Node() = default;
-		Node(GeometricBlock<Key, INode *, EPS, DELTA> *block) : block_(std::move(*block)) {}
-		Collection<INode *>
-		insert(const Key &k, const void *v) override
-		{
-			INode * const *node = static_cast<INode * const *>(v);
-			auto res = block_.insert(k, *node);
-			Collection<INode *> retval;
-			for (size_t i = 0; i < res.size(); ++i) {
-				Node *n = new Node(res[i]);
-				// std::cout << "New node: " << n << std::endl;
-				INode *ptr = static_cast<INode *>(n);
-				// std::cout << "After cast: " << ptr << std::endl;
-				retval.push_back(ptr);
-			}
-			return retval;
-		}
-		bool
-		find(const Key &k, void *v) override
-		{
-			INode **node = static_cast<INode **>(v);
-			return block_.find(k, node);
-		}
-		bool
-		lower_bound(const Key &k, void *v) override
-		{
-			INode **node = static_cast<INode **>(v);
-			return block_.lower_bound(k, node);
-		}
-		bool
-		is_leaf() override
-		{
-			return false;
-		}
-		const Key &
-		origin_key() override
-		{
-			return block_.origin_key();
-		}
-		const Key &
-		start_key() override
-		{
-			return block_.start_key();
-		}
-		INode *
-		first_node() override
-		{
-			return block_.origin_value();
-		}
-		Collection<INode *>
-		get_children() override
-		{
-			return block_.get_values();
-		}
-		Collection<Key>
-		get_data() override
-		{
-			return block_.get_data();
-		}
-		Collection<Key>
-		get_extra() override
-		{
-			return block_.get_extra();
-		}
-		void
-		del(const Key &k) override
-		{
-			return block_.del(k);
-		}
-		void
-		del_checked(const Key &k) override
-		{
-			return block_.del_checked(k);
-		}
-	private:
-		GeometricBlock<Key, INode *, EPS, DELTA> block_;
-	};
+	Collection<Node *>
+	payload_to_nodes(DataPayload *payload)
+	{
+		assert(payload != NULL);
+		Collection<Node *> nodes{};
+		for (size_t i = 0; i < payload->size; ++i)
+			nodes.push_back(new Node(matras_, payload->data[i]));
+		return nodes;
+	}
 
-	struct Leaf : INode {
-		Leaf() = default;
-		Leaf(GeometricBlock<Key, Value, EPS, DELTA> *block) : block_(std::move(*block)) {}
-		Collection<INode *>
-		insert(const Key &k, const void *v) override
-		{
-			const Value *val = static_cast<const Value *>(v);
-			auto res = block_.insert(k, *val);
-			Collection<INode *> retval;
-			for (size_t i = 0; i < res.size(); ++i) {
-				Leaf *l = new Leaf(res[i]);
-				// std::cout << "New leaf: " << l << std::endl;
-				INode *ptr = static_cast<INode *>(l);
-				// std::cout << "After cast: " << ptr << std::endl;
-				retval.push_back(ptr);
-			}
-			return retval;
-		}
-		bool
-		find(const Key &k, void *v) override
-		{
-			Value *val = static_cast<Value *>(v);
-			return block_.find(k, val);
-		}
-		bool
-		lower_bound(const Key &k, void *v) override
-		{
-			Value *val = static_cast<Value *>(v);
-			return block_.lower_bound(k, val);
-		}
-		bool
-		is_leaf() override
-		{
-			return true;
-		}
-		const Key &
-		origin_key() override
-		{
-			return block_.origin_key();
-		}
-		const Key &
-		start_key() override
-		{
-			return block_.start_key();
-		}
-		INode *
-		first_node() override
-		{
-			abort();
-		}
-		Collection<INode *>
-		get_children() override
-		{
-			return {};
-		}
-		Collection<Key>
-		get_data() override
-		{
-			return block_.get_data();
-		}
-		Collection<Key>
-		get_extra() override
-		{
-			return block_.get_extra();
-		}
-		void
-		del(const Key &k) override
-		{
-			return block_.del(k);
-		}
-		void
-		del_checked(const Key &k) override
-		{
-			return block_.del_checked(k);
-		}
-	private:
-		GeometricBlock<Key, Value, EPS, DELTA> block_;
-	};
 public:
 
-	pgdm_map(): root_(new Leaf) {}
+	pgdm_map(matras_alloc_func extent_alloc_func,
+		 matras_free_func extent_free_func,
+		 void *alloc_ctx, struct matras_stats *alloc_stats)
+	{
+		matras_create(&matras_,
+			      EXTENT_SIZE, BLOCK_SIZE,
+			      extent_alloc_func, extent_free_func, alloc_ctx,
+			      alloc_stats);
+		matras_head_read_view(&view_);
+		root_ = new Node(matras_);
+		root_->set_leaf();
+	}
 
 	/**
 	 * If key lower than all other keys - put it in the first one.
+	 * TODO: do not forget to truncate region.
 	 */
 	void
-	insert(const Key &k, const Value &v)
+	insert(const Key &k, void *v)
 	{
 		assert(root_ != NULL);
-		INode *curr = root_;
-		std::stack<INode *> s;
+		Node *curr = root_;
+		std::stack<Node *> s;
 		// std::cout << "------ Starting Insert of " << k << " ------" << std::endl;
 		while (!curr->is_leaf()) {
 			assert(curr != NULL);
 			// std::cout << "Descending to leaf" << std::endl;
 			s.push(curr);
-			INode *next = NULL;
-			bool found = curr->lower_bound(k, &next);
+			Node *next = NULL;
+			Node **next_ptr = &next;
+			bool found = curr->lower_bound(k, (void **)next_ptr);
 			assert(!found || next != NULL);
 			if (!found) {
 				// std::cout << "Lower bound didn't help, getting the first node" << std::endl;
-				next = curr->first_node();
+				next = static_cast<Node *>(curr->origin_value());
 				assert(next != NULL);
 			}
 			assert(next != NULL);
@@ -234,19 +78,25 @@ public:
 		// std::cout << "Leaf has been found" << std::endl;
 		assert(curr != NULL);
 		assert(curr->is_leaf());
-		const void *val_ptr = static_cast<const void *>(&v);
-		Collection<INode *> new_parts = curr->insert(k, val_ptr);
-		// std::cout << "After insert leaf has fallen to " << new_parts.size() << " parts" << std::endl;
-		if (new_parts.empty())
-			return;
-		/*
-		 * TODO: it is not truth actually - since we introduced
-		 * tombstones, number of fragments can be much bigger.
-		 * We should count deleted elemets together with size of
-		 * extra_ to get that guarantee on parts number.
-		 */
-		assert(new_parts.size() <= DELTA + 1);
-		while (!s.empty() && !new_parts.empty()) {
+		Collection<Node *> new_nodes{};
+		{
+			DataPayload* new_parts = curr->insert(k, v);
+			// std::cout << "After insert leaf has fallen to " << new_parts.size() << " parts" << std::endl;
+			if (new_parts == NULL)
+				return;
+			assert(new_parts->size > 0);
+			/*
+			 * TODO: it is not truth actually - since we introduced
+			 * tombstones, number of fragments can be much bigger.
+			 * We should count deleted elemets together with size of
+			 * extra_ to get that guarantee on parts number.
+			 */
+			assert(new_parts->size <= DELTA + 1);
+			new_nodes = payload_to_nodes(new_parts);
+			for (size_t i = 0; i < new_nodes.size(); ++i)
+				new_nodes[i]->set_leaf();
+		}
+		while (!s.empty() && !new_nodes.empty()) {
 			// std::cout << "Start inserting to ancestor " << new_parts.size() << " parts" << std::endl;
 			/*
 			 * Current node has fell apart, so we need to delete
@@ -254,29 +104,36 @@ public:
 			 */
 			auto parent = s.top();
 			parent->del_checked(curr->start_key());
-			Collection<INode *> curr_new_nodes{};
-			curr = s.top();
-			s.pop();
+			Collection<Node *> curr_new_nodes = {};
 			size_t i = 0;
-			for (; i < new_parts.size() && curr_new_nodes.empty(); ++i) {
-				curr_new_nodes =
-					curr->insert(new_parts[i]->origin_key(), &new_parts[i]);
+			{
+				DataPayload *curr_new_parts = NULL;
+				curr = s.top();
+				s.pop();
+				for (; i < new_nodes.size() && curr_new_parts == NULL; ++i) {
+					Node *new_node = new_nodes[i];
+					curr_new_parts =
+						curr->insert(new_node->origin_key(), new_node);
+				}
+				if (curr_new_parts == NULL) {
+					// std::cout << "Stop inserting to ancestor - no additional steps required" << std::endl;
+					new_nodes.clear();
+					break;
+				}
+				assert(curr_new_parts->size > 0);
+				assert(curr_new_parts->size <= DELTA + 1);
+				curr_new_nodes = payload_to_nodes(curr_new_parts);
 			}
-			assert(curr_new_nodes.size() <= DELTA + 1);
-			if (curr_new_nodes.empty()) {
-				// std::cout << "Stop inserting to ancestor - no additional steps required" << std::endl;
-				new_parts.clear();
-				break;
-			}
-			if (i == new_parts.size()) {
+			if (i == new_nodes.size()) {
 				// std::cout << "Keep inserting - the last node fell apart" << std::endl;
-				new_parts = std::move(curr_new_nodes);
+				new_nodes = std::move(curr_new_nodes);
 				continue;
 			}
 			// std::cout << "Keep inserting" << std::endl;
-			for (; i < new_parts.size(); ++i) {
+			for (; i < new_nodes.size(); ++i) {
 				size_t insert_idx = 0;
-				auto &curr_key = new_parts[i]->origin_key();
+				Node *new_node = new_nodes[i];
+				auto &curr_key = new_node->origin_key();
 				while (insert_idx < curr_new_nodes.size() &&
 				       curr_key <= curr_new_nodes[insert_idx]->origin_key()) {
 					insert_idx++;
@@ -284,100 +141,103 @@ public:
 				if (insert_idx > 0)
 					insert_idx--;
 				/* TODO: assert that deletion is redundant? */
-				auto tmp_res = curr_new_nodes[insert_idx]->insert(curr_key, &new_parts[i]);
-				assert(tmp_res.empty());
+				DataPayload *tmp_res = curr_new_nodes[insert_idx]->insert(curr_key, new_node);
+				assert(tmp_res == NULL);
 				(void)tmp_res;
 			}
-			new_parts = std::move(curr_new_nodes);
+			new_nodes = std::move(curr_new_nodes);
 		}
 		/*
 		 * Stack is empty while new_parts is not - that means that
 		 * root of our index has fallen apart. Create a new one.
 		 */
-		if (s.empty() && !new_parts.empty()) {
+		if (s.empty() && !new_nodes.empty()) {
 			// std::cout << "Stack is empty while new_parts is not - " << new_parts.size() << " nodes left" << std::endl;
-			if (new_parts.size() == 1) {
+			if (new_nodes.size() == 1) {
 				// std::cout << "Just re-assign root" << std::endl;
-				root_ = new_parts[0];
+				root_ = new_nodes[0];
 				return;
 			}
-			root_ = new Node;
-			for (size_t i = 0; i < new_parts.size(); ++i) {
-				auto tmp_res =
-					root_->insert(new_parts[i]->origin_key(), &new_parts[i]);
-				assert(tmp_res.empty());
+			root_ = new Node(matras_);
+			for (size_t i = 0; i < new_nodes.size(); ++i) {
+				DataPayload *tmp_res =
+					root_->insert(new_nodes[i]->origin_key(), new_nodes[i]);
+				assert(tmp_res == NULL);
 				(void)tmp_res;
 			}
 		}
 	}
 
 	bool
-	find(const Key &k, Value *v)
+	find(const Key &k, void **v)
 	{
-		INode *curr = root_;
+		Node *curr = root_;
 		while (!curr->is_leaf()) {
-			INode *next;
-			bool found = curr->lower_bound(k, &next);
+			Node *next;
+			Node ** next_ptr = &next;
+			bool found = curr->lower_bound(k, (void **)next_ptr);
 			if (!found)
-				next = curr->first_node();
+				next = static_cast<Node *>(curr->origin_value());
 			curr = next;
 		}
 		return curr->find(k, v);
 	}
 
-	void
-	dump_node(INode *curr, int depth = 0)
-	{
-		auto data = curr->get_data();
-		auto extra = curr->get_extra();
-		auto children = curr->get_children();
-		std::cout << std::endl;
-		std::cout << "Level: " << depth << std::endl;
-		if (curr->is_leaf())
-			std::cout << "-- Leaf --" << std::endl;
-		std::cout << "Data:\n";
-		for (auto &k : data) {
-			std::cout << k << ", ";
-		}
-		std::cout << "\nExtra:\n";
-		for (auto &k : extra) {
-			std::cout << k << ", ";
-		}
-		std::cout << "\nChildren:\n";
-		for (auto &c : children) {
-			std::cout << c << ", ";
-		}
-		std::cout << std::endl << std::endl;
-		for (auto &c : children)
-			dump_node(c, depth + 1);
-	}
+	// void
+	// dump_node(Node *curr, int depth = 0)
+	// {
+	// 	auto data = curr->get_data();
+	// 	auto extra = curr->get_extra();
+	// 	auto children = curr->get_children();
+	// 	std::cout << std::endl;
+	// 	std::cout << "Level: " << depth << std::endl;
+	// 	if (curr->is_leaf())
+	// 		std::cout << "-- Leaf --" << std::endl;
+	// 	std::cout << "Data:\n";
+	// 	for (auto &k : data) {
+	// 		std::cout << k << ", ";
+	// 	}
+	// 	std::cout << "\nExtra:\n";
+	// 	for (auto &k : extra) {
+	// 		std::cout << k << ", ";
+	// 	}
+	// 	std::cout << "\nChildren:\n";
+	// 	for (auto &c : children) {
+	// 		std::cout << c << ", ";
+	// 	}
+	// 	std::cout << std::endl << std::endl;
+	// 	for (auto &c : children)
+	// 		dump_node(c, depth + 1);
+	// }
 
-	void
-	dump()
-	{
-		dump_node(root_);
-	}
+	// void
+	// dump()
+	// {
+	// 	dump_node(root_);
+	// }
 
-	size_t
-	count_node(INode *curr)
-	{
-		auto data = curr->get_data();
-		auto extra = curr->get_extra();
-		if (curr->is_leaf())
-			return data.size() + extra.size();
-		auto children = curr->get_children();
-		size_t retval = 0;
-		for (auto &c : children)
-			retval += count_node(c);
-		return retval;
-	}
+	// size_t
+	// count_node(Node *curr)
+	// {
+	// 	auto data = curr->get_data();
+	// 	auto extra = curr->get_extra();
+	// 	if (curr->is_leaf())
+	// 		return data.size() + extra.size();
+	// 	auto children = curr->get_children();
+	// 	size_t retval = 0;
+	// 	for (auto &c : children)
+	// 		retval += count_node(c);
+	// 	return retval;
+	// }
 
-	size_t	
-	count()
-	{
-		return count_node(root_);
-	}
+	// size_t	
+	// count()
+	// {
+	// 	return count_node(root_);
+	// }
 
 private:
-	INode *root_;
+	struct matras matras_;
+	struct matras_view view_;
+	Node *root_;
 };
