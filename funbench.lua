c000fa3cfa2ac289ccdbbe5bbc90b020e2f03c85
Andrey Saranchin <Andrey22102001@gmail.com>
2024-09-10 16:47:49 +0300

    luafun: add a drop_while benchmark
    The commit introduces new implementations of `drop_while` and
    `funbench.lua` to benchmark them. The benchmark covers only one method
    at a time, there is a comment with list of all implementation - simply
    change called method to cover another one.

---
+++ funbench.lua
@@ -0,0 +1,33 @@
+local clock = require('clock')
+local fun = require('fun')
+
+local t = {}
+
+local N = 1e6
+
+math.randomseed(os.time())
+for i = 1, N do
+    table.insert(t, math.random(50))
+end
+table.insert(t, 100)
+for i = 1, N do
+    table.insert(t, math.random(100))
+end
+
+local acc = 0
+local function bench()
+    for _ = 1, 300 do
+        local factor = math.random(10)
+	-- Methods:
+	-- drop_while - original version
+	-- stateful_drop_while - my version for stateful iterators
+	-- stateful_drop_while2 - version for stateful iterators developed by Alexander Turenko
+        for _, i in fun.iter(t):drop_while(function(x) return x <= 50 end) do
+            acc = acc + i * factor
+        end
+    end
+end
+
+local elapsed = clock.bench(bench)[1]
+
+print(elapsed)
